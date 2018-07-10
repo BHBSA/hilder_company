1ff83683db7b4fd2870de2e3f610f00d99de8f20
jijunyu <jijunyu@fangjia.com>
2018-07-10 11:50:56 +0800

    company

--- company.py
+++ company.py
@@ -1,3 +1,28 @@
+import datetime
+import yaml
+from lib.log import LogHandler
+from lib.mongo import Mongo
+
+log = LogHandler(__name__)
+
+setting = yaml.load(open('config.yaml'))
+client = Mongo(host=setting['mongo']['host'], port=setting['mongo']['port'], user_name=setting['mongo']['user_name'],
+               password=setting['mongo']['password']).connect
+coll = client[setting['mongo']['db']][setting['mongo']['collection']]
+
+
+def check_company(source, company_id):
+    return coll.find_one({'source': source, 'auction_id': str(company_id)})
+
+
+def serialization_info(info):
+    """
+    :param info:
+    :return: data:
+    """
+    return {key: value for key, value in vars(info).items()}
+
+
 class Company:
     def __init__(self, company_id, city=None, region=None, address=None, company_name=None,
                  company_short_info=None, company_info=None, business=None, company_size=None,
@@ -18,3 +43,9 @@ class Company:
         self.development_stage = development_stage  # 经营范围
         self.company_source = company_source  # source
         self.company_id = company_id  # company_id
+
+    def insert_db(self):
+        data = serialization_info(self)
+        data['crawler_time'] = datetime.datetime.now()
+        coll.insert_one(data)
+        log.info('插入数据={}'.format(data))
