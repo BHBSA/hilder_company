bf79264bf1d67fb7082216d9a3d4068ff6a87f1b
jijunyu <jijunyu@fangjia.com>
2018-07-11 11:55:37 +0800

    source_config

--- source_config.py
+++ source_config.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 import yaml
 from lib.log import LogHandler
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import Column, Integer, String, MetaData
 
 log = LogHandler(__name__)
@@ -34,6 +35,7 @@ class City(Base):
     name = Column(String(2000))
     request_parameter = Column(String(2000))
     source = Column(String(2000))
+    url = Column(String(2000))
 
 
 class Region(Base):
@@ -56,6 +58,29 @@ class Block(Base):
     region_id = Column(Integer)
 
 
+def get_sqlalchemy_session(engine=None, autocommit=False, autoflush=True):
+    """
+    获取一个sqlalchemy session
+
+    How to use:
+
+        # 获取一个文章列表
+        from lib.models.article import Article
+        db_session = get_sqlalchemy_session()
+        articles = db_session.query(Article).all()
+        print(articles)
+        db_session.close()
+
+    :return: Object
+    """
+    if not engine:
+        engine = get_sqlalchemy_engine()
+    db_session = scoped_session(sessionmaker(autocommit=autocommit,
+                                             autoflush=autoflush,
+                                             bind=engine))
+    return db_session
+
+
 def get_sqlalchemy_engine():
     """
     获取一个sqlalchemy engine
@@ -67,10 +92,10 @@ def get_sqlalchemy_engine():
                                                  c['password'],
                                                  c['port'],
                                                  c['host'], ))
-    engine.execute("CREATE DATABASE IF NOT EXISTS {0} ".format(c['db_name']))
+    engine.execute("CREATE DATABASE IF NOT EXISTS {0} default character set utf8".format(c['db_name']))
     engine.execute("USE {0}".format(c['db_name']))
     return engine
 
 
-# if __name__ == '__main__':
-#     Base.metadata.create_all(get_sqlalchemy_engine())
+if __name__ == '__main__':
+    Base.metadata.create_all(get_sqlalchemy_engine())
